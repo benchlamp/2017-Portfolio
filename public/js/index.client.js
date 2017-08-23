6e1798ee203c3681a76e6582156f52f2e29b4f46
James <jamesrea83@gmail.com>
2017-08-23 13:21:46 +0000

    projects section populating from projectData

--- public/js/index.client.js
+++ public/js/index.client.js
@@ -1,1 +1,25 @@
-console.log("index.client.js loaded")
+
+
+$(document).ready(function() {
+
+    var projectDataKeys = Object.keys(projectData);    
+    
+    for (var i = projectDataKeys.length -1; i > -1; i--) {
+        var projectID = projectDataKeys[i];
+        $(".recent").after(
+            
+                "<div class='section project'>" +
+                    "<div class='project-thumbnail'>" +
+                        "<div class='project-cover' id='" + projectID + "'><h4>Click for more info</h4></div>" +
+                        "<img src='" + projectData[projectDataKeys[i]].thumbnail + "' />" +
+                    "</div>" +
+                    "<div class='project-caption'>" + projectData[projectDataKeys[i]].title + "</div>" +
+                "</div>"
+            
+        )
+
+    }
+    
+
+})
+    
